851d395c95bd9aee97ca90a10e8b4c18c703db8d
Polizzi12 <peppepolizzi73@gmail.com>
2024-06-27 20:19:02 +0200

    aggiunta footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import '../App.css'; 
-import Inter from '../assets/internobar.jpg'
+import Interno from '../assets/internobar.jpg'
+import Esterno from '../assets/esternobar.jpg'
 import { Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
@@ -19,8 +20,8 @@ function Home() {
      <div className='card'>
       <div className="row mm">
         <div className="col-md-6">
-          <Card style={{ height: '100%' }}>
-            <Card.Img variant="top" src={Inter} />
+          <Card style={{ height: '100%'  }}>
+            <Card.Img variant="top" src={Interno}  className="card-img-top"/>
           </Card>
         </div>
         <div className="col-md-6">
@@ -28,7 +29,7 @@ function Home() {
           <Card style={{ height: '100%' }}>
             <Card.Body>
               <Card.Title className='card-title'>
-                      Benvenuti al Bar Concordia!
+                      Benvenuti Al Bar Concordia!
               </Card.Title>
               <Card.Text>
               Situato a Mazzarrone, in Piazza della Concordia, il nostro bar è il luogo ideale per gustare la miglior pasticceria del paese e molto altro ancora. Da noi potrete assaporare dolci freschi e prelibatezze artigianali che vi conquisteranno. Non è solo una questione di dolcezza: al Bar Concordia troverete un ambiente accogliente dove trascorrere piacevoli momenti tra amici e famiglia. Un luogo dove la tradizione si unisce alla qualità, offrendo sempre il meglio per ogni occasione. Venite a trovarci e scoprite il gusto autentico del nostro bar, nel cuore di Mazzarrone.
@@ -66,6 +67,32 @@ function Home() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+    </div>
+    <div className="container-fluid melo" >
+     <div className='card'>
+      <div className="row mm">
+        <div className="col-md-6  order-2 order-md-1">
+        <Card style={{ height: '100%', }}>
+            <Card.Body className='did'>
+              <Card.Title className='card-title'>
+                      Esplora La Nostra Pasticceria
+              </Card.Title>
+              <Card.Text>
+              Immergiti nel dolce mondo della nostra pasticceria, dove ogni morso ti farà assaporare la genuinità e la passione delle nostre creazioni. Torte spettacolari, pasticcini deliziosi e altre prelibatezze ti aspettano. Vieni a scoprire il piacere di un dolce fatto con amore.
+              </Card.Text>
+              <Link to="/Prodotti">
+                <Button className="bottone">PASTICCERIA</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-6  order-1 order-md-2" >
+        <Card style={{ height: '100%'}}>
+            <Card.Img variant="top" src={Esterno}  className="card-img-top"/>
+          </Card>
+        </div>
+        </div>
+      </div>
     </div>
 </div>
   );
